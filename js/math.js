2adb73edc32ac70ecd59d518cd80c4b8082e966c
calmosowl <calmos.owl@gmail.com>
2017-09-04 12:43:57 +0300

    update

--- js/math.js
+++ js/math.js
@@ -1,72 +1,12 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", (ev) => {
 	(()=>{
-let log = document.getElementById('log');
-function getLog(msg) {
-    log.textContent += msg + "\n";
-    var ot = log.scrollHeight - log.clientHeight;
-    if (ot > 0) log.scrollTop = ot;
-}
-
-var math = new MathMashine({
-	min: 0,
-	max: 20,
-	octothorpe: ['+', '-', '*']
-});
-
-var example = document.querySelectorAll('.example');
-example = Array.prototype.slice.call(example);
-function examination() {
-	var input = example[i].children[4];
-    input.onkeydown = (ev) => {
-		if(ev.keyCode == 13){
-			if(input.value == input.getAttribute('result')) 
-				input.setAttribute('style', 'color: rgba(10, 242, 101, 0.4)');
-			else 
-				input.setAttribute('style', 'color: rgba(242, 10, 10, 0.4)');
-		}
-	}
-}
-/*==========================*/
-for (var i = 0; i < example.length; i++) {
-	math.setNums(example[i].children[0]);
-	math.setNums(example[i].children[2]);
-	math.setOctothorpe(example[i].children[1]);
-
-	switch((example[i].children[1]).textContent) {
-		case '+': 
-			var result = +example[i].children[0].textContent + +example[i].children[2].textContent;
-			example[i].children[4].setAttribute('result', result);
-
-		break
-
-		case '-':
-			var result = +example[i].children[0].textContent - +example[i].children[2].textContent;
-			example[i].children[4].setAttribute('result', result);
-		break
-
-		case '*':
-			var result = +example[i].children[0].textContent * +example[i].children[2].textContent;
-			example[i].children[4].setAttribute('result', result);
-		break
-
-		case '/':
-			var result = +example[i].children[0].textContent / +example[i].children[2].textContent;
-			example[i].children[4].setAttribute('result', result);
-		break
-
-		default:
-			var result = +example[i].children[0].textContent + +example[i].children[2].textContent;
-			example[i].children[4].setAttribute('result', result);
-		break
-	}
-	examination();
-	//console.log( input.value + ': ' + input.getAttribute('result') );
-}
-/*=========================*/
-
-
-})();
+		let math = new MathMashine({
+			min: 0,
+			max: 10,
+			octothorpe: ['+', '-']
+		});
+	})();
 }, false);
 
 function MathMashine(options){
@@ -74,13 +14,39 @@ function MathMashine(options){
 	this.max = options&&options.max ? options.max: 10;
 	this.min =  options&&options.min ? options.min: 0;
 	this.octothorpe = options&&options.octothorpe ? options.octothorpe: ['+', '-'];
-	this.numsPrototype = [0,0];
-	this.data = {
-		nums: [],
-		octothorpe: '',
-		result: 0,
-		stamp: []
-	};
+	this.example;
+	this.inputs;
+	this.result=[];
+	let examination = () => {
+		that.input = Array.from(document.querySelectorAll('.result'));
+		let width = Math.max(...that.result).toString();
+		that.input.forEach((item, i) => {
+		    item.onkeydown = (ev) => {
+				if(ev.keyCode == 13){
+					if(item.value == item.getAttribute('result')) {
+						item.style.color = 'rgba(10, 242, 101, 0.4)';
+						that.input[i + 1].focus();
+					}
+
+					else 
+						item.style.color = 'rgba(242, 10, 10, 0.4)';
+				}
+			}
+		item.style.width = width.length + 'ch';  
+		});
+		that.input[0].focus();
+	}
+
+	let gen = () => {
+		for (var i = 0; i < that.example.length; i++) {
+			that.setNums(that.example[i].children[0]);
+			that.setNums(that.example[i].children[2]);
+			that.setOctothorpe(that.example[i].children[1]);
+			that.setResult(that.example[i]);
+			that.example[i].children[3].textContent = ' = ';
+			examination();
+		}
+	}
 
 	function random(min, max) {
 		var rand = min - 0.5 + Math.random() * (max - min + 1)
@@ -94,25 +60,7 @@ function MathMashine(options){
     	return operatorSign;
 	}
 	
-	function getStamp(obj) {
-  		for (var key in obj) {
-			that.data.stamp.push( obj[key] );
-		}
-    return that.data.stamp;
-}
 	
-	this.data.nums = this.numsPrototype.map(function(item) {
-  		return item = random(that.min, that.max);
-	});
-
-	//this.data.octothorpe = returnThis("+", "-");
-	
-	this.data.result = this.data.nums.reduce(function(sum, current) {
-  		return sum + current;
-	});
-
-	getStamp(this.data);
-
 	this.setNums = (el) => {
 		el.textContent = random(that.min, that.max);
 	}
@@ -120,10 +68,31 @@ function MathMashine(options){
 	this.setOctothorpe = (el) => {
 		el.textContent = randomOctothorpe(that.octothorpe);
 	}
-}
 
-/*O_O_O_O_O*/
+	this.setResult = (el, str) => {
+		let result = eval(el.textContent);
+		el.lastElementChild.setAttribute('result', result);
+		that.result.push(result);
+	};
 
-function isPositive(number) {
-  return number > 0;
+	(this.draw = () => {
+		let container = document.getElementById('section');
+
+			let column = document.createElement('div');
+				column.className = 'column';
+			
+			let drawingCells = "<div class='example'><span class='num' data-num=''></span><span class='operator'></span><span class='num' data-num=''></span><span class='operator equal'></span><input type='text' class='result' result=''></div>";
+		
+			let collection = '';
+			for (let i = 5; i > 0; i--) {
+				collection += drawingCells;
+			}
+			column.innerHTML = collection;
+			container.appendChild(column);
+			let clone = column.cloneNode(true);
+			container.appendChild(clone);
+			that.example = Array.from(document.querySelectorAll('.example'));
+			gen();
+			
+	})();
 }
